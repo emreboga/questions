27e0ead19ecf663bd7f2130b9b4d6568c276d60f
Emre B <emreboga@stubhub.com>
2014-11-22 00:35:15 -0800

    First 2 questions in binary tree

---
+++ binary-trees/1.js
@@ -0,0 +1,12 @@
+/*
+Given a binary tree, populate the nextRight pointers in each node.
+You may assume that it is a full binary tree (ie, each node other than the leaves has two children.)
+*/
+
+var q1 = (function() {
+    return {
+        exec: function() {
+            alert('first called..');
+        }
+    };
+}());
---
+++ binary-trees/2.js
@@ -0,0 +1,11 @@
+/*
+Given a binary tree, find its maximum height.
+*/
+
+var q2 = (function() {
+    return {
+        exec: function() {
+            alert('second called..');
+        }
+    };
+}());
