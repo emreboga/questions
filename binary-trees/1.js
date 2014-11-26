b20b99e26e7457c9aa2a75ba36071112cf7ca14b
Emre B <emreboga@stubhub.com>
2014-11-26 12:01:19 -0800

    Finishing up the first question

--- binary-trees/1.js
+++ binary-trees/1.js
@@ -1,12 +1,118 @@
 /*
 Given a binary tree, populate the nextRight pointers in each node.
 You may assume that it is a full binary tree (ie, each node other than the leaves has two children.)
+var node = {
+    'left': '',
+    'right': '',
+    'nextRight': ''
+};
 */
 
 var q1 = (function() {
+    // Sample tree
+    var tree = {
+        id: '0',
+        left: {
+            id: '1',
+            left: {
+                id: '3',
+                left: {
+                    id: '7'
+                },
+                right: {
+                    id: '8'
+                }
+            },
+            right: {
+                id: '4',
+                left: {
+                    id: '9'
+                },
+                right: {
+                    id: '10'
+                }
+            }
+        },
+        right: {
+            id: '2',
+            left: {
+                id: '5',
+                left: {
+                    id: '11'
+                },
+                right: {
+                    id: '12'
+                }
+            },
+            right: {
+                id: '6',
+                left: {
+                    id: '13'
+                },
+                right: {
+                    id: '14'
+                }
+            }
+        }
+    };
+    // Queue implementation
+    function Queue() {
+        this.q = [];
+    }
+    Queue.prototype.enqueue = function(item) {
+        this.q.unshift(item);
+    };
+    Queue.prototype.dequeue = function() {
+        return this.q.splice(this.q.length - 1, 1)[0];
+    };
+    Queue.prototype.isEmpty = function() {
+        return this.q.length === 0;
+    };
+    Queue.prototype.peek = function() {
+        return this.q[this.q.length - 1];
+    };
+    function insertNextRight(node) {
+        // Variable decleration
+        var q,
+            lastAddedNode;
+
+        // Check if node or both children are not defined
+        if (!node || (!node.right && !node.left)) {
+            return;
+        }
+
+        // Init queue, set root node's incLevel and lastAddedNode
+        q = new Queue();
+        q.enqueue(node);
+        node.incLevel = true;
+        lastAddedNode = node;
+
+        // Loop as long as the queue is not empty
+        while (!q.isEmpty()) {
+            // Get the next in the queue
+            var n = q.dequeue();
+            // Check left and right to enqueue accordingly
+            if (n.left) {
+                q.enqueue(n.left);
+                lastAddedNode = n.left;
+            }
+            if (n.right) {
+                q.enqueue(n.right);
+                lastAddedNode = n.right;
+            }
+            // Check if level needs to be incremented
+            // set nextRight otherwise
+            if (!n.incLevel) {
+                n.nextRight = q.peek();
+            } else {
+                lastAddedNode.incLevel = true;
+            }
+        }
+        return node;
+    }
     return {
         exec: function() {
-            return 'First question solution';
+            return insertNextRight(tree);
         }
     };
 }());
