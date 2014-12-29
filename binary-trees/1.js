795d06268fa7902eefe294636fd6565ca74431c5
Emre B <emreboga@stubhub.com>
2014-12-29 00:10:54 -0800

    Move the sample tree to common.js

--- binary-trees/1.js
+++ binary-trees/1.js
@@ -9,52 +9,6 @@ var node = {
 */
 
 var q1 = (function() {
-    // Sample tree
-    var tree = {
-        id: '0',
-        left: {
-            id: '1',
-            left: {
-                id: '3',
-                left: {
-                    id: '7'
-                },
-                right: {
-                    id: '8'
-                }
-            },
-            right: {
-                id: '4',
-                left: {
-                    id: '9'
-                },
-                right: {
-                    id: '10'
-                }
-            }
-        },
-        right: {
-            id: '2',
-            left: {
-                id: '5',
-                left: {
-                    id: '11'
-                },
-                right: {
-                    id: '12'
-                }
-            },
-            right: {
-                id: '6',
-                left: {
-                    id: '13'
-                },
-                right: {
-                    id: '14'
-                }
-            }
-        }
-    };
     // Queue implementation
     function Queue() {
         this.q = [];
---
+++ binary-trees/core.js
@@ -0,0 +1,46 @@
+// Sample tree
+var tree = {
+    id: '0',
+    left: {
+        id: '1',
+        left: {
+            id: '3',
+            left: {
+                id: '7'
+            },
+            right: {
+                id: '8'
+            }
+        },
+        right: {
+            id: '4',
+            left: {
+                id: '9'
+            },
+            right: {
+                id: '10'
+            }
+        }
+    },
+    right: {
+        id: '2',
+        left: {
+            id: '5',
+            left: {
+                id: '11'
+            },
+            right: {
+                id: '12'
+            }
+        },
+        right: {
+            id: '6',
+            left: {
+                id: '13'
+            },
+            right: {
+                id: '14'
+            }
+        }
+    }
+};
